0cb9cc2a948aa6c4a6d38e8b05b69405eb118bdf
D Sai Ganesh <ganemcos@gmail.com>
2017-12-30 06:05:50 +0530

    getting data
    Getting data from usgs website

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -25,39 +26,66 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static Myadaptor madaptor;
 
+    private static final String Usgs_Request_Url =
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+
+    private static class EarthQuakeAsynkTask extends AsyncTask<String,Void,List<EarthQuakes>> {
+
+        @Override
+        protected List<EarthQuakes> doInBackground(String... Urls) {
+
+            if (Urls.length < 1 || Urls == null) {
+                return null;
+            }
+            List<EarthQuakes> result = QueryUtils.fletchEarthquakes(Urls[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<EarthQuakes> data) {
+            madaptor.clear();
+
+            if (data!=null && !data.isEmpty())
+            {
+                madaptor.addAll(data);
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        final ArrayList<EarthQuakes> earthquakes = QueryUtils.extractEarthquakes();
 
-
-        // Create a new {@link ArrayAdapter} of earthquakes
-        Myadaptor adapter = new Myadaptor(this, earthquakes);
-
-        // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        madaptor = new Myadaptor(this,new ArrayList<EarthQuakes>());
 
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(madaptor);
+
+        EarthQuakeAsynkTask task = new EarthQuakeAsynkTask();
+        task.execute(Usgs_Request_Url);
+
+
+
+
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent urlrequestintent  = new Intent(Intent.ACTION_VIEW);
-                EarthQuakes quakes = earthquakes.get(position);
-                String url = quakes.geturl();
-                urlrequestintent.setData(Uri.parse(url));
-                startActivity(urlrequestintent);
+                EarthQuakes currentEarthQuake = (EarthQuakes) madaptor.getItem(position);
+                Uri earthquakeuri = Uri.parse(currentEarthQuake.geturl());
+                Intent websiteintent = new Intent(Intent.ACTION_VIEW,earthquakeuri);
+                startActivity(websiteintent);
             }
         });
 
